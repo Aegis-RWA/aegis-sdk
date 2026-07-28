3f9bad5bdabadefed03fc73806a51d7f75b3b52c
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2026-07-28 14:28:08 +0100

    feat: add SDK role discovery and capability checks (#46)
    * feat: add SDK role discovery and capability checks (#31)
    
    * chore: remove accidental package-lock.json

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@ import { rpc, Keypair, Networks } from '@stellar/stellar-sdk';
 import { ComplianceModule } from './compliance';
 import { AssetModule } from './asset';
 import { InvestorModule } from './investor/portfolio';
+import { RoleModule } from './role';
 
 export interface AegisClientConfig {
   rpcUrl: string;
@@ -20,6 +21,7 @@ export class AegisClient {
   public compliance: ComplianceModule;
   public asset: AssetModule;
   public investor: InvestorModule;
+  public role: RoleModule;
 
   /**
    * Initializes the Aegis RWA SDK Client.
@@ -36,6 +38,7 @@ export class AegisClient {
     this.compliance = new ComplianceModule(this);
     this.asset = new AssetModule(this);
     this.investor = new InvestorModule(this);
+    this.role = new RoleModule(this);
   }
 
   /**
